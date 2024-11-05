f469f19a56a44f2db32fed8b250f947f90ebb610
Your Name <you@example.com>
2024-11-05 10:39:18 -0800

    published 1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+}
+
+module.exports = nextConfig
+module.exports = {
   output: 'export',
   images: {
     unoptimized: true,
   },
   basePath: '/naveen-portfolio', 
 };
-
-module.exports = nextConfig;
